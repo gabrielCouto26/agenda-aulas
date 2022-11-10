9e748ee080d009dc41d0312a59f56ddedf3a23df
Gabriel Couto <gabrielcouto26@gmail.com>
2022-11-10 02:17:00 -0300

    corrige users controller e implementa responses

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,29 +3,64 @@ class UsersController < ApplicationController
   
   def index
     @users = User.all
+
+    if @users
+      render json: { status: 200, data: @users }
+    else
+      render json: { status: 500, data: "Falha ao buscar usuários" }
+    end
   end
   
   def show
-    @user = User.find(params[:id])
+    @user = User.where(id: params[:id])
+
+    if @user.any?
+      render json: { status: 200, data: @user.first }
+    else
+      render json: { status: 404, data: "Usuário não encontrado" }
+    end
   end
   
   def create
     @user = User.create(user_params)
 
     if @user.save
-      render json: { status: 200, user: @user }
+      render json: { status: 200, data: @user }
     else 
-      render json: { status: 500, user: nil }
+      render json: { status: 500, data: "Falha ao cadastrar usuário" }
     end
   end
   
   def update
-    @user = User.find(params[:id])
-    @user.update(user_params)
+    @user = User.where(id: params[:id])
+    
+    if @user.any?
+      @user.first.update(user_params)
+      
+      if @user.first.save
+        render json: { status: 200, data: @user.first }
+      else 
+        render json: { status: 500, data: "Falha ao editar usuário" }
+      end
+    else
+      render json: { status: 404, data: "Usuário não encontraedo" }
+    end
   end
   
   def destroy
-    User.find([params[:id]]).destroy
+    @user = User.where(id: params[:id])
+
+    if @user.any?
+      @user.first.destroy
+      
+      if @user.first.destroyed?
+        render json: { status: 204, data: nil }
+      else 
+        render json: { status: 500, data: "Falha ao remover usuário" }
+      end
+    else
+      render json: { status: 404, data: "Usuário não encontraedo" }
+    end
   end
 
   private

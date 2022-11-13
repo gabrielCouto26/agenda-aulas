5fa886a560565c416c9daedba0196e8506ecbca6
Gabriel de Lima Couto <gabriel.couto@hitbel.com.br>
2022-11-13 00:25:54 -0300

    implementa teste relaciomanto teacher e classrooms

--- test/models/teacher_test.rb
+++ test/models/teacher_test.rb
@@ -45,12 +45,38 @@ class TeacherTest < ActiveSupport::TestCase
     assert teacher.destroyed?
   end
 
+  test "teacher has many classrooms" do
+    user = create_user
+    teacher = Teacher.create(user_id: user.id)
+    subject = create_subject
+    classroom1 = create_classroom(teacher.id, subject.id)
+    classroom2 = create_classroom(teacher.id, subject.id)
+    
+    assert classroom1.save
+    assert classroom2.save
+    assert_not_empty teacher.classrooms
+    assert_equal 2, teacher.classrooms.size
+  end
+
   def create_user
     User.create(
       name: "gabriel",
       email: "gabriel@email.com",
       password: "1234",
       birth_date: "26/09/1996"
+    )
+  end
+
+  def create_subject
+    Subject.create(
+      name: "História"
+    )
+  end
+
+  def create_classroom(teacher_id, subject_id)
+    Classroom.create(
+      teacher_id: teacher_id,
+      subject_id: subject_id
     )
   end
 end

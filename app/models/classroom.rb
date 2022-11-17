6683cf0f99b899daf9b6edec3505dda5cf861760
Gabriel Couto <gabrielcouto26@gmail.com>
2022-11-16 23:04:46 -0300

    adiciona dependent: :destroy

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -2,5 +2,5 @@ class Classroom < ApplicationRecord
   belongs_to :subject, optional: true
   belongs_to :teacher, optional: true
   has_and_belongs_to_many :students
-  has_one :class_details
+  has_one :class_details, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :profile
-  has_one :teacher
-  has_one :student
-  has_one :address
+  has_many :profile, dependent: :destroy
+  has_one :teacher, dependent: :destroy
+  has_one :student, dependent: :destroy
+  has_one :address, dependent: :destroy
 end

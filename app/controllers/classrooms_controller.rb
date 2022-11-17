b09b210d1c15254e16623cdd64df1254e78e4e11
Gabriel Couto <gabrielcouto26@gmail.com>
2022-11-17 00:52:32 -0300

    implementa ação de teacher e student para classrooms

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -2,14 +2,9 @@ class ClassroomsController < ApplicationController
   skip_before_action :verify_authenticity_token  
   
   def index
-    if params[:student_id]
-      student = Student.where(id: params[:student_id]).first
-      if student.present?
-        @classrooms = student.classrooms
-        render json: { status: 200, data: @classrooms }
-      else
-        render json: { status: 404, data: "Aluno não encontrado" }
-      end
+    if params[:student_id] or params[:teacher_id]
+      student_classrooms if params[:student_id]
+      teacher_classrooms if params[:teacher_id]
     else
       @classrooms = Classroom.all
       if @classrooms
@@ -62,23 +57,9 @@ class ClassroomsController < ApplicationController
   end
   
   def destroy
-    if params[:student_id]
-      student = Student.where(id: params[:student_id]).first
-      if student.present?
-        @classroom = student.classrooms.where(id: params[:id]).first
-        if @classroom.present?
-          @classroom.destroy
-          if @classroom.destroyed?
-            render json: { status: 204, data: nil }
-          else 
-            render json: { status: 500, data: "Falha ao remover classe" }
-          end
-        else
-          render json: { status: 404, data: "Classe não encontrada" }
-        end
-      else
-        render json: { status: 404, data: "Aluno não encontrado" }
-      end
+    if params[:student_id] or params[:teacher_id]
+      student_destroy_classroom if params[:student_id]
+      teacher_destroy_classroom if params[:teacher_id]
     else
       @classroom = Classroom.where(id: params[:id]).first
 
@@ -102,6 +83,28 @@ class ClassroomsController < ApplicationController
       params.require(:classroom).permit(:teacher_id, :subject_id, :student_id, :name)
     end
 
+    def student_classrooms
+      student = Student.where(id: params[:student_id]).first
+      
+      if student.present?
+        @classrooms = student.classrooms
+        render json: { status: 200, data: @classrooms }
+      else
+        render json: { status: 404, data: "Aluno não encontrado" }
+      end
+    end
+
+    def teacher_classrooms
+      teacher = Teacher.where(id: params[:teacher_id]).first
+      
+      if teacher.present?
+        @classrooms = teacher.classrooms
+        render json: { status: 200, data: @classrooms }
+      else
+        render json: { status: 404, data: "Professor não encontrado" }
+      end
+    end
+
     def student_create(id, classrooms_params)
       student = Student.where(id: id).first
       if student.present?
@@ -121,6 +124,44 @@ class ClassroomsController < ApplicationController
           subject_id: classrooms_params[:subject_id],
           name: classrooms_params[:name])
         render json: { status: 200, data: @classroom }
+      else
+        render json: { status: 404, data: "Professor não encontrado" }
+      end
+    end
+
+    def student_destroy_classroom
+      student = Student.where(id: params[:student_id]).first
+      if student.present?
+        @classroom = student.classrooms.where(id: params[:id]).first
+        if @classroom.present?
+          @classroom.destroy
+          if @classroom.destroyed?
+            render json: { status: 204, data: nil }
+          else 
+            render json: { status: 500, data: "Falha ao remover classe" }
+          end
+        else
+          render json: { status: 404, data: "Classe não encontrada" }
+        end
+      else
+        render json: { status: 404, data: "Aluno não encontrado" }
+      end
+    end
+
+    def teacher_destroy_classroom
+      teacher = Teacher.where(id: params[:teacher_id]).first
+      if teacher.present?
+        @classroom = teacher.classrooms.where(id: params[:id]).first
+        if @classroom.present?
+          @classroom.destroy
+          if @classroom.destroyed?
+            render json: { status: 204, data: nil }
+          else 
+            render json: { status: 500, data: "Falha ao remover classe" }
+          end
+        else
+          render json: { status: 404, data: "Classe não encontrada" }
+        end
       else
         render json: { status: 404, data: "Professor não encontrado" }
       end

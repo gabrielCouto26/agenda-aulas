8e679d7146b1f8fb732659bb25d0e610e1be9802
Gabriel Couto <gabrielcouto26@gmail.com>
2022-11-18 23:31:37 -0300

    corrige class_detail

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -2,5 +2,5 @@ class Classroom < ApplicationRecord
   belongs_to :subject, optional: true
   belongs_to :teacher, optional: true
   has_and_belongs_to_many :students
-  has_one :class_details, dependent: :destroy
+  has_one :class_detail, dependent: :destroy
 end
